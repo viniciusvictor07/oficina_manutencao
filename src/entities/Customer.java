164d550b327cd11e56b05bee2ac7473fe0f97a8e
viniciusvictor07 <srzeldris12@gmail.com>
2026-05-14 12:48:49 -0300

    refactor: remover atributos de preço de serviço da classe Customer

--- src/entities/Customer.java
+++ src/entities/Customer.java
@@ -2,13 +2,9 @@ package entities;
 
 public class Customer {
     private String name;
-    private String model;
-    private Double modelPrice;
 
-    public Customer(String name, String model, Double modelPrice) {
+    public Customer(String name) {
         this.name = name;
-        this.model = model;
-        this.modelPrice = modelPrice;
     }
 
     public String getName() {
@@ -17,21 +13,5 @@ public class Customer {
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public String getModel() {
-        return model;
-    }
-
-    public void setModel(String model) {
-        this.model = model;
-    }
-
-    public Double getModelPrice() {
-        return modelPrice;
-    }
-
-    public void setModelPrice(Double modelPrice) {
-        this.modelPrice = modelPrice;
     }
 }
