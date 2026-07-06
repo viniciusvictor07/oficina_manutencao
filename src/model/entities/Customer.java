635c29ebe7952920b95ebd6d409ee5f1e856840e
viniciusvictor07 <srzeldris12@gmail.com>
2026-07-06 17:05:19 -0300

    chore: remover método obsoleto

--- src/model/entities/Customer.java
+++ src/model/entities/Customer.java
@@ -1,38 +1,20 @@
 package model.entities;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Customer {
     private int id;
     private final String customerName;
-    private final List<ServiceOrder> customerOrders;
 
     public Customer(String customerName) {
         this.customerName = customerName;
-        this.customerOrders = new ArrayList<>();
     }
 
     public Customer(int id, String customerName) {
         this.id = id;
         this.customerName = customerName;
-        this.customerOrders = new ArrayList<>();
-    }
-
-    public boolean addServiceOrder(ServiceOrder pendingServiceOrder) {
-        return this.customerOrders.add(pendingServiceOrder);
-    }
-
-    public boolean hasServices() {
-        return !this.customerOrders.isEmpty();
     }
 
     public int getId() {
         return id;
-    }
-
-    public List<ServiceOrder> getCustomerOrders() {
-        return customerOrders;
     }
 
     public String getCustomerName() {
