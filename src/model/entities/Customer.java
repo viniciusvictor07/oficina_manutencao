9f81de82852f81624a4a8341aad6777c6a1349d1
viniciusvictor07 <srzeldris12@gmail.com>
2026-08-15 12:36:21 -0300

    feat: adicionar mapeamento JPA na classe Customer

--- src/model/entities/Customer.java
+++ src/model/entities/Customer.java
@@ -1,8 +1,25 @@
 package model.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_customer")
+
 public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
     private Integer id;
-    private final String customerName;
+    private String customerName;
+
+    public Customer() {
+    }
 
     public Customer(String customerName) {
         this.customerName = customerName;
@@ -17,8 +34,16 @@ public class Customer {
         return id;
     }
 
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
     public String getCustomerName() {
         return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
     }
 
     @Override
@@ -26,11 +51,11 @@ public class Customer {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Customer customer = (Customer) o;
-        return java.util.Objects.equals(customerName, customer.customerName);
+        return Objects.equals(id, customer.id);
     }
 
     @Override
     public int hashCode() {
-        return java.util.Objects.hash(customerName);
+        return Objects.hash(id);
     }
 }
